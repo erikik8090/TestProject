2c62cf5d78578ca6dafce947f5d1807f407794f4
erikik8090 <erik8090@gmail.com>
2017-12-30 21:37:32 +0200

    Added a filter feature

--- testsite/rabbits_rest/views.py
+++ testsite/rabbits_rest/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rabbits_rest.models import Rabbit
 from rabbits_rest.serializers import RabbitSerializer
 from rest_framework import viewsets
@@ -6,3 +7,5 @@ from rest_framework import viewsets
 class RabbitViewSet(viewsets.ModelViewSet):
     serializer_class = RabbitSerializer
     queryset = Rabbit.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('Color', 'Nickname', 'Age', 'Breed')
