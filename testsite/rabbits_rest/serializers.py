f6c306caa2ba7bc6b2aa678e2feef5aa5f3cfc74
erikik8090 <erik8090@gmail.com>
2017-12-30 21:38:41 +0200

    Added data validation

--- testsite/rabbits_rest/serializers.py
+++ testsite/rabbits_rest/serializers.py
@@ -1,8 +1,23 @@
 from rabbits_rest.models import Rabbit
 from rest_framework import serializers
+from os.path import abspath, join
 
 
 class RabbitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rabbit
         fields = '__all__'
+
+    def validate_Breed(self, value):
+        with open(join('rabbits_rest', 'data', 'breeds.txt'), 'r') as f:
+            breeds = f.readlines()
+        if value.lower() not in [x.lower().strip() for x in breeds]:
+            raise serializers.ValidationError("Breed does not exist")
+        return value
+
+    def validate_Color(self, value):
+        with open(abspath(join('rabbits_rest', 'data', 'colors.txt')), 'r') as f:
+            colors = f.readlines()
+        if value.lower() not in [x.lower().strip() for x in colors]:
+            raise serializers.ValidationError("Color does not exist")
+        return value
